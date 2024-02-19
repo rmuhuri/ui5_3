8bfb41e50e2aa5969f7e172ba2e4c0e9cf6e750f
Rajarshi Muhuri <rmuhuri@gmail.com>
2024-02-19 13:53:28 -0500

    x

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,23 +1,28 @@
 sap.ui.define(['sap/ui/core/mvc/Controller'],
-    function (Controller) {
-        return Controller.extend('mickey.controller.Main', {
-            callMe: function () {
-                //In JSON views you can do this 
-                var oCore = sap.ui.getCore();
-                var oBtn = oCore.byId("idSpiderman");
-                oBtn.setText('xyz');
-                //get the changed text
-                var z = oBtn.getText();
-                alert(z);
-            },
-            anotherFX: function(){
-              //XML View function, Notice the id of inputfield is appended by the name of the view and --
-                var x = sap.ui.getCore().byId("idMainXML--idInp").getValue();
-                alert(x);  
-              // in XML view if you want to avoid the earlier messy situation , do this  
-                var oView = this.getView();
-                var z =  oView.byId("idInp").getValue();
-                alert(z); 
-            }
-        });
+  function (Controller) {
+    return Controller.extend('mickey.controller.Main', {
+      //global varibale, any function of the controller can use this.
+      rishi: 100,
+      onInit: function () {
+        this.rishi = this.rishi + 100;
+        console.log("global variable + 100 = " + this.rishi);
+      },
+      onBeforeRendering: function(){
+        
+       this.getView().byId("idSal").setValue("999");
+    },
+      onAfterRendering: function () { 
+      $("#idMainXML--idSal").fadeOut(5000).fadeIn(1000).fadeOut(100);
+      console.log('hello');
+      },
+
+
+      
+      
+      anotherFX: function () {
+        var oView = this.getView();
+        var z = oView.byId("idInp").getValue();
+        alert(z);
+      }
     });
+  });
